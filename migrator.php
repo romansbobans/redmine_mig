5d647a5dde0f7693b4d03bfe234b8662afaab834
Romans Stepanovs <romans.stepanovs@citadele.lv>
2016-07-22 13:51:31 +0300

    updated priority mapping

--- migrator.php
+++ migrator.php
@@ -52,12 +52,10 @@ class migrator
 
     private $prioritiesMapping = array(
             1 => 2,
-            2 => 2,
-            3 => 3,
-            4 => 4,
-            5 => 5,
-            6 => 6,
-            7 => 7,
+            2 => 4,
+            3 => 5,
+            4 => 6,
+            5 => 7,
         );
 
     private $statusMapping = array(
